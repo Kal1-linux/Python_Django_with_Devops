03497820ab2491e6301fa18bb4cf246772ff32f4
Pratham Modi <84131564+Kal1-linux@users.noreply.github.com>
2024-01-18 14:12:42 +0530

    Update settings.py
    settings.py changes

--- Company_Review_System/CompanyReviewSystem/settings.py
+++ Company_Review_System/CompanyReviewSystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8zc*r(ivjn#_vt+^5ed00@j6-od3asa3*tiv(*sw6__=r7&+q#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','13.127.204.70','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','13.127.204.70:3000','127.0.0.1']
 
 
 # Application definition
