52c59e059896fd09c7ec3e560e65712476451226
Pratham Modi <84131564+Kal1-linux@users.noreply.github.com>
2024-01-18 14:51:34 +0530

    Update automate.sh
    changes automate.sh

--- Company_Review_System/automate.sh
+++ Company_Review_System/automate.sh
@@ -1,5 +1,5 @@
-docker image rm $(docker image ls -q) --force
-docker build -t dev_auto .
-docker stop dev
+docker kill dev
 docker rm dev
+docker image rm dev_auto:latest
+docker build -t dev_auto:latest .
 docker run -itd --name dev -p 3000:8080 dev_auto:latest
